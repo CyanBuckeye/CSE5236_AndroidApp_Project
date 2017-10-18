df94bfe5fc8e169cc8aa6a39ac7b6951f5d26278
Andrew <xu.2727@osu.edu>
2017-10-18 15:10:30 -0400

    Add files via upload

--- app/src/main/java/com/example/courseproject/choosetopic.java
+++ app/src/main/java/com/example/courseproject/choosetopic.java
@@ -3,6 +3,7 @@ package com.example.courseproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class choosetopic extends AppCompatActivity {
@@ -10,7 +11,26 @@ public class choosetopic extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("choosetopicActivity", "choosetopic_oncreate");
         setContentView(R.layout.activity_choosetopic);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d("choosetopicActivity", "onResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d("choosetopicActivity", "onPause");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("choosetopicActivity", "onDestroy");
     }
 
     protected void onClick_goback(View view){
